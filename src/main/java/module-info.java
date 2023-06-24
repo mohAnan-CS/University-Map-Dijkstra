715f0dcea436c1a8fb59036b9dd416dc09e94e83
Mohammad Anan <smoker_shuger@hotmail.com>
2023-06-25 01:20:13 +0300

    Add building points to the map

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module com.example.birzeituniversitymapdijkstra {
+
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
 
-
-
     opens com.birzeit.birzeituniversitymapdijkstra to javafx.fxml, javafx.graphics;
     exports com.birzeit.birzeituniversitymapdijkstra.controller;
+
 }
