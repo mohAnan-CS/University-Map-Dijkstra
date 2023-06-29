8c59508d6c479a2b2c6e40432ae0d74bfd250918
Mohammad Anan <smoker_shuger@hotmail.com>
2023-06-29 19:08:27 +0300

    Collect building names with x and y cordinate

--- src/main/java/com/birzeit/birzeituniversitymapdijkstra/controller/MapController.java
+++ src/main/java/com/birzeit/birzeituniversitymapdijkstra/controller/MapController.java
@@ -9,10 +9,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MapController implements Initializable {
@@ -28,19 +31,49 @@ public class MapController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-
+        System.out.println("Distance = " + calculateDistance(619, 501,507, 584));
 
         anchorPane.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-            String buildingName = textFiled.getText();
-            double x = event.getX();
-            double y = event.getY();
-            System.out.println("Building Name : " +  buildingName +  " at (" + x + ", " + y + ")");
-            Building building = new Building(buildingName, x, y);
-            arrayList.add(building);
+            if (!textFiled.getText().trim().equalsIgnoreCase("")) {
+                String buildingName = textFiled.getText();
+                double x = event.getX();
+                double y = event.getY();
+                System.out.println("Building Name : " + buildingName + " at (" + x + ", " + y + ")");
+                Building building = new Building(buildingName, x, y);
+                arrayList.add(building);
+                textFiled.setText("");
+            }
         });
 
 
 
+    }
+
+    public List<Building> readBuildingDataFromFile(String filename) {
+        List<Building> buildings = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(", ");
+                if (data.length == 3) {
+                    String buildingName = data[0];
+                    double x = Double.parseDouble(data[1]);
+                    double y = Double.parseDouble(data[2]);
+                    Building building = new Building(buildingName, x, y);
+                    buildings.add(building);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
+        }
+
+        return buildings;
+    }
+
+
+    public double calculateDistance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     @FXML
